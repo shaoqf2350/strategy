6a824f9824e1ebe0c3a8a5f808f0a20853327e82
shaoqf2350 <781350666@qq.com>
2024-01-21 16:40:29 +0800

    优化架构

--- src/main/java/com/shaoqf/strategy/service/importStrategy/JournalStrategy.java
+++ src/main/java/com/shaoqf/strategy/service/importStrategy/JournalStrategy.java
@@ -2,6 +2,7 @@ package com.shaoqf.strategy.service.importStrategy;
 
 import com.shaoqf.strategy.base.ImportStrategy;
 import com.shaoqf.strategy.utils.enums.ImportType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,33 +10,43 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * 日记账导入
  */
-@Primary
+@Slf4j
 @Service("JOURNAL")
 public class JournalStrategy implements ImportStrategy {
 
     @Override
     public String checkFormat(double amount) {
-        return ImportType.JOURNAL.getDesc().concat(", 验证数据格式及表头:").concat(String.valueOf(amount));
+        String str = ImportType.JOURNAL.getDesc().concat(", 验证数据格式及表头:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 
     @Override
     public String readFile(double amount) {
-        return ImportType.JOURNAL.getDesc().concat(", 读取数据:").concat(String.valueOf(amount));
+        String str = ImportType.JOURNAL.getDesc().concat(", 读取数据:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 
     @Override
     public String handleData(double amount) {
-        return ImportType.JOURNAL.getDesc().concat(", 处理数据:").concat(String.valueOf(amount));
+        String str = ImportType.JOURNAL.getDesc().concat(", 处理数据:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 
     @Override
     public String convert(double amount) {
-        return ImportType.JOURNAL.getDesc().concat(", 转换Bean到通用Bean:").concat(String.valueOf(amount));
+        String str = ImportType.JOURNAL.getDesc().concat(", 转换Bean到通用Bean:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 
     @Override
     @Transactional(rollbackFor=Exception.class)
     public String saveDB(double amount) {
-        return ImportType.JOURNAL.getDesc().concat(", 保存数据库:").concat(String.valueOf(amount));
+        String str = ImportType.JOURNAL.getDesc().concat(", 保存数据库:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 }
--- src/main/java/com/shaoqf/strategy/service/importStrategy/VoucherStrategy.java
+++ src/main/java/com/shaoqf/strategy/service/importStrategy/VoucherStrategy.java
@@ -2,6 +2,7 @@ package com.shaoqf.strategy.service.importStrategy;
 
 import com.shaoqf.strategy.base.ImportStrategy;
 import com.shaoqf.strategy.utils.enums.ImportType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -10,33 +11,43 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * 凭证导入
  */
-@Primary
+@Slf4j
 @Service("VOUCHER")
 public class VoucherStrategy implements ImportStrategy {
 
     @Override
     public String checkFormat(double amount) {
-        return ImportType.VOUCHER.getDesc().concat(", 验证数据格式及表头:").concat(String.valueOf(amount));
+        String str = ImportType.VOUCHER.getDesc().concat(", 验证数据格式及表头:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 
     @Override
     public String readFile(double amount) {
-        return ImportType.VOUCHER.getDesc().concat(", 读取数据:").concat(String.valueOf(amount));
+        String str = ImportType.VOUCHER.getDesc().concat(", 读取数据:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 
     @Override
     public String handleData(double amount) {
-        return ImportType.VOUCHER.getDesc().concat(", 处理数据:").concat(String.valueOf(amount));
+        String str = ImportType.VOUCHER.getDesc().concat(", 处理数据:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 
     @Override
     public String convert(double amount) {
-        return ImportType.VOUCHER.getDesc().concat(", 转换Bean到通用Bean:").concat(String.valueOf(amount));
+        String str = ImportType.VOUCHER.getDesc().concat(", 转换Bean到通用Bean:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 
     @Override
     @Transactional(rollbackFor=Exception.class)
     public String saveDB(double amount) {
-        return ImportType.VOUCHER.getDesc().concat(", 保存数据库:").concat(String.valueOf(amount));
+        String str = ImportType.VOUCHER.getDesc().concat(", 保存数据库:").concat(String.valueOf(amount));
+        log.info(str);
+        return str;
     }
 }
